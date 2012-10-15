efc4c8b2e051422f6e6a2f897097e77741052072
Charlie Yuan <charlie@klout.com>
2012-10-14 17:57:14 -0700

    details endpoint

--- sample.js
+++ sample.js
@@ -5,13 +5,19 @@ var GoogleAPI = require('./lib/node-google-api');
 var gapi = new GoogleAPI({key: key.key});
 
 
-var params = {
+var searchParams = {
     longitude: '0'
   , latitude: '0'
-  , radius: 1000
+  , radius: 5000
   , sensor: false
 };
 
-gapi.place.search(params, function (e, d) {
+var detailsParams = {
+    reference: 'CnRsAAAA98C4wD-VFvzGq-KHVEFhlHuy1TD1W6UYZw7KjuvfVsKMRZkbCVBVDxXFOOCM108n9PuJMJxeAxix3WB6B16c1p2bY1ZQyOrcu1d9247xQhUmPgYjN37JMo5QBsWipTsnoIZA9yAzA-0pnxFM6yAcDhIQbU0z05f3xD3m9NQnhEDjvBoUw-BdcocVpXzKFcnMXUpf-nkyF1w'
+  , sensor: false
+  , language: 'en'
+};
+
+gapi.place.details(detailsParams, function (e, d) {
     console.log(arguments);
 });
